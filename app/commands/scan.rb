0ac8a5ee39706ebeadb27b23a6d2356e52819f3c
James Wozniak <wozza35@hotmail.com>
2026-04-06 00:24:12 +0100

    Display detailed information for each found zone player

--- app/commands/scan.rb
+++ app/commands/scan.rb
@@ -1,5 +1,6 @@
 require_relative "base"
 require_relative "../ssdp/scanner"
+require_relative "../sonos/device_description"
 
 module Commands
   class Scan < Base
@@ -17,9 +18,36 @@ module Commands
 
       if locations.empty?
         puts "No devices found."
-      else
-        puts "Found #{locations.size} device(s):"
-        locations.each { |location| puts "  #{location}" }
+        return
+      end
+
+      locations.each_with_index do |location, i|
+        puts "\n#{i + 1}:"
+        puts " #{location}"
+        zone_player = Sonos::DeviceDescription.new(location).fetch
+        display_zone_player(zone_player)
+      end
+    end
+
+    private
+
+    def display_zone_player(zone_player)
+      puts "  Room: #{zone_player.room_name} "
+      puts "  Display name: #{zone_player.display_name}"
+      puts "  Model: #{zone_player.model_name} (#{zone_player.model_number})"
+
+      puts "  Services:"
+      zone_player.services.each do |service|
+        puts "    #{service.name}"
+      end
+
+      puts "  Embedded devices:"
+      zone_player.embedded_devices.each do |device|
+        puts "    #{device.model_description}"
+        puts "    Services:"
+        device.services.each do |service|
+          puts "      #{service.name}"
+        end
       end
     end
   end
