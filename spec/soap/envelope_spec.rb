2c2a9624567f43a937e7ab0845c7be4f41c498ec
James Wozniak <wozza35@hotmail.com>
2026-04-09 22:43:38 +0100

    Adds SOAP::Envelope class

---
+++ spec/soap/envelope_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../../app/soap/envelope'
+
+describe SOAP::Envelope do
+  let(:namespace) { 'urn:schemas-upnp-org:service:Example:1' }
+  let(:operation) { 'TestAction' }
+  let(:envelope) { described_class.new(operation, namespace) }
+
+  describe '#to_xml' do
+    subject { envelope.to_xml }
+
+    it 'generates the correct XML' do
+      expected_xml = <<~XML
+        <?xml version="1.0" encoding="utf-8"?>
+        <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+          <s:Body>
+            <u:TestAction xmlns:u="urn:schemas-upnp-org:service:Example:1"/>
+          </s:Body>
+        </s:Envelope>
+      XML
+      expect(subject).to eq(expected_xml)
+    end
+  end
+end
